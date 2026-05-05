7fc2076bb802d9c9d071141115c63a099dcad43a
tjcouch-sil <tj_couch@sil.org>
2026-05-05 01:00:49 -0500

    Re-export frame APIs from gbcam-extract

---
+++ packages/gbcam-extract/src/frames/index.ts
@@ -0,0 +1,4 @@
+export type { Frame } from "./types.js";
+export { splitSheet } from "./split-sheet.js";
+export { dedupeFrames } from "./dedupe.js";
+export { composeFrame } from "./compose.js";
--- packages/gbcam-extract/src/index.ts
+++ packages/gbcam-extract/src/index.ts
@@ -28,6 +28,8 @@ export {
   ADDITIONAL_PALETTES,
   FUN_PALETTES,
 } from "./data/palettes-generated.js";
+export type { Frame } from "./frames/types.js";
+export { splitSheet, dedupeFrames, composeFrame } from "./frames/index.js";
 
 import type { GBImageData, PipelineResult, PipelineOptions } from "./common.js";
 import { locate } from "./locate.js";
